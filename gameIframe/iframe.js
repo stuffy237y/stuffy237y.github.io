ffe35b563eb215fac07b2d6d50afffa53fad042c
stuffy237y <160800065+stuffy237y@users.noreply.github.com>
2024-02-25 18:19:39 -0800

    Update iframe.js

--- gameIframe/iframe.js
+++ gameIframe/iframe.js
@@ -1,9 +1,11 @@
 var fragmentIdentifier = window.location.hash.slice(1)
 
 var iframe = document.createElement('iframe');
-
+if (fragmentIdentifier === "minecraft/1.5.2/index.html"){
+    iframe.src = "https://stuffy237y.github.io/minecraft/1.5.2/index.html"
+} else {
 iframe.src = "https://stuffy237y.github.io/games/" + fragmentIdentifier
-
+}
 iframe.style.border='none';
 iframe.style.width = '100%' ;
 iframe.style.height = "100vh"
