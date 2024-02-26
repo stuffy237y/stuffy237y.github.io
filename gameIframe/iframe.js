b8313e78a3c34d03e9ae9bf889f01554fe34ca8b
stuffy237y <160800065+stuffy237y@users.noreply.github.com>
2024-02-25 18:13:49 -0800

    Add files via upload

---
+++ gameIframe/iframe.js
@@ -0,0 +1,17 @@
+var fragmentIdentifier = window.location.hash.slice(1)
+
+var iframe = document.createElement('iframe');
+
+iframe.src = "https://stuffy237y.github.io/games/" + fragmentIdentifier
+
+iframe.style.border='none';
+iframe.style.width = '100%' ;
+iframe.style.height = "100vh"
+iframe.style.margin = '0';
+
+document.body.appendChild(iframe);
+
+var back = document.getElementById('return')
+back.addEventListener('click', ()=>{
+    window.location.href = "https://stuffy237y.github.io"
+})
