4793ef5da8fb92f9dfd2a2c3d1b14d573c2dc52a
reading-labs <161535478+reading-labs@users.noreply.github.com>
2024-02-27 16:11:56 -0800

    Update iframe.js

--- gameIframe/iframe.js
+++ gameIframe/iframe.js
@@ -2,7 +2,7 @@ var fragmentIdentifier = window.location.hash.slice(1)
 
 var iframe = document.createElement('iframe');
 
-iframe.src = "https://reading-labs.github.io/games" + fragmentIdentifier
+iframe.src = "https://reading-labs.github.io/games/" + fragmentIdentifier
 
 iframe.style.border='none';
 iframe.style.width = '100%' ;
